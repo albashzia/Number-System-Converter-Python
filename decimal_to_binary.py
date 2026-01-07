2a6b29be1cd4de4b180c08ca7bc1d8c65f2bb443
Ahmad Albash Zia <ahmedalbashzia@gmail.com>
2026-01-07 21:24:49 +0500

    Implement decimal to binary conversion function

---
+++ decimal_to_binary.py
@@ -0,0 +1,17 @@
+def decimal_to_binary(num):
+    binary = ""
+    number = num
+
+    if number == 0:
+        return "0"
+
+    while number > 0:
+        remainder = number % 2
+        binary = str(remainder) + binary
+        number = number // 2
+
+    return binary
+
+num = int(input("Enter a decimal number: "))
+result = decimal_to_binary(num)
+print("The number in binary in",result)
