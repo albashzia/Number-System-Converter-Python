7104bfb6d16524622c91ca9756db4222ad147b43
Ahmad Albash Zia <ahmedalbashzia@gmail.com>
2026-01-08 18:14:59 +0500

    Enhance binary_to_decimal.py with comments
    Added comments for clarity and improved code readability.

--- binary_to_decimal.py
+++ binary_to_decimal.py
@@ -1,11 +1,13 @@
+# function to take a binary string and return a decimal number of it
 def binary_to_decimal(string):
-    decimal_number = 0
-
+    decimal_number = 0 # initializing the string
+    # looping over the whole input string
     for digit in string:
         decimal_number = decimal_number * 2 + int(digit)
-
+    # returning the decimal number
     return decimal_number
 
-string = input("Enter a binary number: ")
-result = binary_to_decimal(string)
-print("The number in decimal is",result)
+# main program
+string = input("Enter a binary number: ") # taking input from the user
+result = binary_to_decimal(string) # call to function 
+print("The number in decimal is",result) # print the result 
